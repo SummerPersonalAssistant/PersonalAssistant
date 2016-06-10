d5e1f5808a775a17d4afedb34433d1d563dc873f
An Phu Dupont <anphu.dupont@gmail.com>
2016-06-10 21:06:39 +0200

    C# Speech test

---
+++ PersonalAssistantCS/PersonalAssistantCS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Threading;
+
+namespace PersonalAssistantCS
+{
+    class Program
+    {
+        private static bool _loop = true;
+        static void Main(string[] args)
+        {
+            SummerListener summer = new SummerListener(PrintText);
+            Console.WriteLine("Summer is now online.");
+            while (_loop) { Thread.Sleep(100); }
+        }
+
+        static void PrintText(string s)
+        {
+            if (s == "exit") _loop = false;
+            Console.WriteLine(s);
+        }
+    }   
+}
---
+++ PersonalAssistantCS/PersonalAssistantCS/SummerListener.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech;
+using System.Speech.Recognition;
+
+namespace PersonalAssistantCS
+{
+    class SummerListener
+    {
+        private SpeechRecognitionEngine _recognizer;
+        private Action<string> _listenCallback;
+
+        public SummerListener(Action<string> listenCallback)
+        {
+            _recognizer = null;
+            _recognizer = new SpeechRecognitionEngine();
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammar(new DictationGrammar());
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            _listenCallback = listenCallback;
+        }
+
+        // The callback called when a sentence is recognized
+        private void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string text = e.Result.Text;
+            _listenCallback.Invoke(text);
+        }
+    }
+}
